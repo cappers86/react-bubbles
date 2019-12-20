ea1771b4030ccf2da7137c88747ac67fa0fa40ed
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-20 08:39:49 +0000

    login page created with post request and form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,77 @@
-import React from "react";
+// step 3 Login
 
-const Login = () => {
+import React, {useState} from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+    const [credentials, setCredentials] = useState({
+      username: '',
+      password: '',
+    });
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+
+
+      axiosWithAuth()
+        .post('/login', credentials)
+        .then(result => {
+          console.log('post login', result.data)
+          localStorage.setItem('token', result.data.payload);
+          props.history.push('/bubblepage');
+          console.log('login', props)
+        })
+        .catch(error => 
+          console.log('login post error', error));
+        
+  };
+      
+
+ const hanleChange = (e) => {
+   setCredentials({
+     ...credentials,
+     [e.target.name] : e.target.value
+   })
+ };
+
+    
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      
+      <h1>Login in to your Bubble App!</h1>
+      <form onSubmit = {onSubmit}>
+          <input
+              type ='text'
+              name ='username'
+              placeholder = 'username'
+              value ={credentials.username}
+              onChange = {hanleChange}
+              />
+
+              <br></br>
+
+              <input
+                  type ='password'
+                  name ='password'
+                  placeholder = 'password'
+                  value ={credentials.password}
+                  onChange = {hanleChange}
+                  />
+                    <br></br>
+
+                  <button>login</button>
+      </form>
+      
     </>
   );
 };
 
 export default Login;
+
+
